6b3659d61801d12c8c2d527090e7f9e057cdaa6d
rwoodall <wdalfarm@woodalldevelopment.com>
2024-12-19 11:22:10 -0600

    commit=core

--- Core/ApiProcessingResult.cs
+++ Core/ApiProcessingResult.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,40 +8,9 @@ namespace Core
 {
     public class ApiProcessingResult
     {
-        private bool _IsError = false;
-        private List<ApiProcessingError> _Errors = new List<ApiProcessingError>();
-    
-        public bool IsError {
-            get {
-                IsSuccessful = !_IsError;
-                return _IsError;
-            }
-            set {
-                _IsError = value;
-                IsSuccessful = !_IsError;
-            }
-        }
-
-        public List<ApiProcessingError> Errors {
-            get { 
-                try {
-                    if (_Errors.Count > 0) {
-                        Error = new ProcessingError(_Errors[0].ErrorMessage, _Errors[0].ErrorMessage, false, false);
-                    }
-                   
-                } catch {
-                }
-                
-                return _Errors;
-            }
-            set {
-                _Errors = value;
-                Error = new ProcessingError(_Errors[0].ErrorMessage, _Errors[0].ErrorMessage, false, false);
-            }
-        }
-
-        public bool IsSuccessful { get; set; }
-        public ProcessingError Error { get; set; }
+        public bool CallFailed { get; set; } = false;
+        public bool IsError { get; set; } = false;
+        public List<ApiProcessingError> Errors { get; set; } = new List<ApiProcessingError>();
     }
 
     public class ApiProcessingResult<TReturnedData> : ApiProcessingResult
